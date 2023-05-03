530b2e726cbf5496de3c4b1e2dd9452d6d12bb46
mohamedAtefBadr <atefm0790@gmail.com>
2023-05-03 16:42:02 +0300

    add picture page done

--- lib/pages/add_pic.dart
+++ lib/pages/add_pic.dart
@@ -2,3 +2,25 @@ import 'package:flutter/material.dart';
 
 import '../widgets/colors.dart';
 
+class Addpost extends StatefulWidget {
+  const Addpost({super.key});
+
+  @override
+  State<Addpost> createState() => _AddpostState();
+}
+
+class _AddpostState extends State<Addpost> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      body: Center(
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.upload,
+          size: 55,)
+          ),
+      ),
+    );
+  }
+}
