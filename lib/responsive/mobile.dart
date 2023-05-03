3e2fbe38f708b4d3b4205abdb47ba610b766e376
mohamedAtefBadr <atefm0790@gmail.com>
2023-05-03 16:34:35 +0300

    mobile (responsive)

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_project/widgets/colors.dart';
+
+class MobileScreen extends StatefulWidget {
+  const MobileScreen({super.key});
+
+  @override
+  State<MobileScreen> createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends State<MobileScreen> {
+  final PageController _pageController = PageController();
+
+int currentpage=0;
+@override
+void dispose() {
+_pageController.dispose();
+super.dispose();
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+      // title: const Text("mobile screen"),
+      //),
+      bottomNavigationBar: CupertinoTabBar(
+        onTap:(index) {
+          _pageController.jumpToPage(index);
+          setState(() {
+            currentpage=index;
+          });
+        },
+        backgroundColor: mobileBackgroundColor,
+        items:  [
+        BottomNavigationBarItem(icon: Icon(Icons.home,color : currentpage ==0 ? primaryColor:secondaryColor,),label: "home"),
+        BottomNavigationBarItem(icon: Icon(Icons.search,color:currentpage ==1 ? primaryColor:secondaryColor,),label: "search"),
+        BottomNavigationBarItem(icon: Icon(Icons.add_circle,color: currentpage ==2 ? primaryColor:secondaryColor),label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite,color: currentpage ==3 ? primaryColor:secondaryColor,),label: "favorite"),
+        BottomNavigationBarItem(icon: Icon(Icons.person,color: currentpage ==4 ? primaryColor:secondaryColor,),label: "profile"),
+      ]),
+      body: PageView(
+        onPageChanged: (index){},
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        
+      ),
+    );
+  }
+}
