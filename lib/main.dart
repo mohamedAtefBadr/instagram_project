f810ef60e58c4ac187084ed46ca0eea538c78aa9
mohamedAtefBadr <atefm0790@gmail.com>
2023-05-03 16:03:38 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_project/pages/sign_in.dart';
 
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter',
-      
+      theme: ThemeData.dark(),
+      home: const Login(),
     );
   }
 }
