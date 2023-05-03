add8275a951422b1efdef1159f432cb5f4024283
mohamedAtefBadr <atefm0790@gmail.com>
2023-05-03 16:46:24 +0300

    search page done

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_project/widgets/colors.dart';
 
+class Search extends StatefulWidget {
+  const Search({super.key});
 
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(decoration: const InputDecoration(labelText: "search for a user")),
+      ),
+    );
+  }
+}
